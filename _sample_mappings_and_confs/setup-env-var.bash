5e88d355f8fab1e25be8d4609d7917ae9aaa87b2
Evan Carlin <evan@carlin.com>
2024-11-22 08:45:15 -0800

    Fix #1: Use ecarlin path

--- _sample_mappings_and_confs/setup-env-var.bash
+++ _sample_mappings_and_confs/setup-env-var.bash
@@ -1,4 +1,4 @@
 export LCLS_LATTICE=/sdf/group/ad/sw/scm/repos/optics/lcls-lattice/
-export SCRATCH=/sdf/home/m/mokun/SCRATCH
-export LUME_IMPACT_CODEBASE_LOCATION=/sdf/home/m/mokun/code/lume-repos/lume-impact-live-demo
-export LUME_OUTPUT_FOLDERS=/sdf/home/m/mokun/lume-output
+export SCRATCH=/sdf/home/e/ecarlin/SCRATCH
+export LUME_IMPACT_CODEBASE_LOCATION=/sdf/home/e/ecarlin/lume-live/lume-impact-live-demo/
+export LUME_OUTPUT_FOLDERS=/sdf/group/ad/sw/lume/radiasoft/sirepo-alpha/srv/slactwin_db/lume-impact-live
